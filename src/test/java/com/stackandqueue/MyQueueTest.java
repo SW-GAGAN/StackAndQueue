de81a2a3ef72166cf6a79ec3d12887b5fe7c0e61
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-02 21:51:09 +0530

    To dequeue for Beginning

--- src/test/java/com/stackandqueue/MyQueueTest.java
+++ src/test/java/com/stackandqueue/MyQueueTest.java
@@ -2,11 +2,11 @@ package com.stackandqueue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-/* To create a Queue of 56->30->70 */
+/* To create a Queue of 56->30->70.
+ *  To dequeue from the beginning.*/
 public class MyQueueTest {
     @Test
-    public void given3IntegersWhenAddedToQueueShouldPassTheTest() {
+    public void given3IntegersWhenAddedToQueueShouldPassTheTest(){
         MyQueue myQueue = new MyQueue();
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
@@ -17,5 +17,20 @@ public class MyQueueTest {
         INode peak = myQueue.peak();
         myQueue.printQueue();
         Assertions.assertEquals(peak, myFirstNode);
+    }
+
+    @Test
+    public void given3IntegersInQueueWhenDequeueShouldHaveFirstAddedNode(){
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+        INode dequeue = myQueue.dequeue();
+        myQueue.printQueue();
+        Assertions.assertEquals(dequeue, myFirstNode);
     }
 }
